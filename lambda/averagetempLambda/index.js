f8d42a9cb633e9e63e7cb75522fbe6b0faa23cc8
Dirk van Dooren <dirk.vanDooren@cloudnation.nl>
2020-04-17 13:36:56 +0200

    added mongodb stuff in lambda, password needs to be reset after completion

--- lambda/averagetempLambda/index.js
+++ lambda/averagetempLambda/index.js
@@ -1,1 +1,51 @@
-const hello = "world"
+"use strict";
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URI = process.env.MONGODB_URI; // or Atlas connection string
+
+let cachedDb = null;
+
+function connectToDatabase (uri) {
+  console.log('=> connect to database');
+
+  if (cachedDb) {
+    console.log('=> using cached database instance');
+    return Promise.resolve(cachedDb);
+  }
+  let auth ={
+        user: "dirk",
+        password: "QQ65eKZzzYYq943O"
+  }
+  return MongoClient.connect(uri, {"auth":auth})
+    .then(db => {
+      cachedDb = db;
+      return cachedDb;
+    });
+}
+
+function queryDatabase (db) {
+  console.log('=> query database');
+
+  return db.collection('items').find({}).toArray()
+    .then(() => { return { statusCode: 200, body: 'success' }; })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      return { statusCode: 500, body: 'error' };
+    });
+}
+
+module.exports.handler = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  console.log('event: ', event);
+
+  connectToDatabase(MONGODB_URI)
+    .then(db => queryDatabase(db))
+    .then(result => {
+      console.log('=> returning result: ', result);
+      callback(null, result);
+    })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      callback(err);
+    });
+};
